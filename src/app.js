b76f9551a80e9a8d754f4f4b30f045fd1e995e63
PixelStudents <cianoneil2@gmail.com>
2026-02-19 22:35:42 +0000

    Add app.js

--- src/app.js
+++ src/app.js
@@ -111,8 +111,7 @@ function formatCountdown(ms) {
   const hh = String(Math.floor(s / 3600)).padStart(2, "0");
   const mm = String(Math.floor((s % 3600) / 60)).padStart(2, "0");
   const ss = String(s % 60).padStart(2, "0");
-  return \
-`\${hh}:\${mm}:\${ss}`;
+  return `\${hh}:\${mm}:\${ss}`;
 }
 
 function deviceInfo() {
