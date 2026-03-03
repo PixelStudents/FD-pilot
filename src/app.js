2fda0b2274edef0ba7934f4127477374c6e877d8
PixelStudents <cianoneil2@gmail.com>
2026-03-03 13:06:08 +0000

    Fix template literals in getTodayKeyUTC function

--- src/app.js
+++ src/app.js
@@ -151,7 +151,7 @@ function median(arr) {
 // --------------------
 function getTodayKeyUTC() {
   const d = new Date();
-  return `\${d.getUTCFullYear()}-\${String(d.getUTCMonth() + 1).padStart(2, "0")} -\${String(d.getUTCDate()).padStart(2, "0")}\`;
+  return `${d.getUTCFullYear()}-${String(d.getUTCMonth() + 1).padStart(2, "0")} -${String(d.getUTCDate()).padStart(2, "0")}`;
 }
 
 function getSessionCountToday(aliasHash) {
