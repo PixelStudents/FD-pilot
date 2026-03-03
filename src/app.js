895982e95a754e14cd34ecdd225af5dcfebb4636
PixelStudents <cianoneil2@gmail.com>
2026-03-03 13:59:59 +0000

    Correct return statement in time formatting function
    Fix syntax error in return statement formatting.

--- src/app.js
+++ src/app.js
@@ -111,7 +111,7 @@ function formatCountdown(ms) {
   const hh = String(Math.floor(s / 3600)).padStart(2, "0");
   const mm = String(Math.floor((s % 3600) / 60)).padStart(2, "0");
   const ss = String(s % 60).padStart(2, "0");
-  return \
+  return
     `${hh}:${mm}:${ss}`;
 }
 
